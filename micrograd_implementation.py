a8235fc3687bf9be0b12037eea443e529641dc71
lukasanakingod <64533041+lukasanakingod@users.noreply.github.com>
2023-07-26 15:57:35 +0200

    micrograd implementation AK

---
+++ micrograd_implementation.py
@@ -0,0 +1,166 @@
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Value:
+    def __init__(self, data, _children=(), _op='', label =''):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+        return out
+    
+    def __rmul__(self, other):
+        return self * other 
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __neg__(self):
+        return self * -1 
+
+    def __sub__(self, other):
+        return self + (-other)
+
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other *(self.data**(other-1)) * out.grad
+        
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1-t**2)*out.grad
+
+        out._backward = _backward
+        return out
+
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+
+        out._backward = _backward
+        return out
+    
+
+    def backward(self):
+
+        # topological sort för att bara gå igenom allting vänster till höger (bakifrån)
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        topo
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
+    
+    def __call__(self, x):
+        # <w, x> + b
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+
+        return out
+    
+    def parameters(self):
+        return self.w + [self.b]
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+    
+class MLP:
+    
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+      
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
+
+n = MLP(3, [4, 4, 1])
+xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0]
+]
+ys = [1.0, -1.0, -1.0, 1.0]
+
+for k in range(20):
+    #forward pass
+    ypred = [n(x) for x in xs]
+    loss = sum((yout-ygt)**2 for ygt, yout in zip(ys, ypred))
+    for p in n.parameters():
+        p.grad = 0.0
+    #backward pass
+    loss.backward()
+    #update
+    for p in n.parameters():
+        p.data += -0.1 * p.grad
+    print(k, loss)
